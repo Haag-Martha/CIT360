71e497598863312ef66a310e1c91126d6230c699
Haag-Martha <haa18001@byui.edu>
2019-10-08 18:07:09 -0400

    Fixes to Java Collections

--- Java Collections/src/Collections.java
+++ Java Collections/src/Collections.java
@@ -1,180 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package cit360;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.TreeSet;
 
 /*
  @author martha
  */
-public class CollectionsExample {
+public class Collections {
 
     public static void main(String[] args) {
-        //Array Example
-        System.out.println("*****Example of an Array*****");
-        ArrayList<String> listofcountries = new ArrayList<>();
-        listofcountries.add("Ireland");
-        listofcountries.add("United Kingdom");
-        listofcountries.add("Scotland");
-        listofcountries.add("United States of America");
-        listofcountries.add("Canada");
-        listofcountries.add("Mexico");
-        listofcountries.add("Russia");
+        // TODO code application logic here
         
-        //Display Current ArrayList
-                System.out.println("Origial Array List:");
-        for(String counter: listofcountries) {
-                System.out.println(counter);
-        }
-        listofcountries.remove("Russia");
-        listofcountries.remove(3);
+        // ArrayList example
+        List one = new ArrayList();
+        one.add("Amy");
+        one.add("Naomi");
+        one.add("Becky");
+        one.add("Matthew");
+        one.add("Gail");
+        System.out.println("\n\nLists Example: \n");
+        System.out.println("My Friends");
         
-        //Display Removed List
-                System.out.println("\nCurrent Array List:");
-        for(String counter: listofcountries) {
-                System.out.println(counter);
+        Iterator friends = one.iterator();
+        while(friends.hasNext()) {
+        Object element = friends.next();
+        System.out.print(element + "\n");
         }
         
-        //Display Unsorted List
-                System.out.println("\nBefore Sorting:");
-        for(String counter: listofcountries) {
-                System.out.println(counter);
+        System.out.print("\n");        
+        
+        // Set example
+        System.out.println("Set Collections Example");
+        int advance[] = {2,9,4,0,3,1,5};
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < 7; i++) {
+            set.add(advance[i]);
         }
+        System.out.println(set + "\n");
+
+        //  Map example
+        System.out.println("HashMap Example");
+        Map hmap = new HashMap();
+        hmap.put("George Washington", "1789 - 1797");
+        hmap.put("John Adams", "1797 - 1801");
+        hmap.put("Thomas Jefferson", "1801 - 1809");
+        hmap.put("James Madison", "1809 - 1825");
+        hmap.put("John Quincy Adams", "1825 - 1829");
+
+        System.out.println("The first presidents of the United were: \n");
+
+        hmap.forEach((k,v) -> System.out.println("President - " + k 
+                + ", Presided from " + v + "\n"));
+
+        // Tree example
+        System.out.println("TreeSet Example. With Trees!");
+        TreeSet baum = new TreeSet();
+        baum.add("Magnolia");
+        baum.add("Elm");
+        baum.add("Oak");
+        baum.add("Pine");
+        baum.add("Maple");
+        baum.add("Ash");
+        baum.add("Cherry");
         
-        //Sort statement
-        Collections.sort(listofcountries);
-        
-        //Display Sorted List
-                System.out.println("\nAfter Sorting:");
-        for(String counter: listofcountries) {
-                System.out.println(counter);
+        Iterator list = baum.iterator();
+        while(list.hasNext()) {
+        Object element = list.next();
+        System.out.print(element + "\n");
         }
-        
-        System.out.println("\n*****Example of a LinkedList*****");
-        //LinkedList Example
-        LinkedList<String> linkedlist = new LinkedList<String>();
-        linkedlist.add("Item3");
-        linkedlist.add("Item9");
-        linkedlist.add("Item16");
-        linkedlist.add("Item0");
-        linkedlist.add("Item5");
-        
-        //Display LinkedList items
-                System.out.println("\nOrignial LinkedList:");
-        for(String counter: linkedlist) {
-                System.out.println(counter);
-        }
-        
-        //Add First and Last
-        linkedlist.addFirst("First Item");
-        linkedlist.addLast("Last Item");
-        
-        //Display LinkedList items
-                System.out.println("\nFirst/Last Item LinkedList:");
-        for(String counter: linkedlist) {
-                System.out.println(counter);
-        }
-        
-        //Get and Set values in the list
-        String firstItem = linkedlist.get(0);
-                System.out.println("\nFirst element is: " + firstItem);
-        linkedlist.set(0, "Changed First Item");
-        firstItem = linkedlist.get(0);
-                System.out.println("First element is: " + firstItem);
-        
-        //Remove first and last items in a list
-        linkedlist.removeFirst();
-        linkedlist.removeLast();
-                System.out.println("\nRemove First/Last LinkedList:");
-        for(String counter: linkedlist) {
-                System.out.println(counter);
-        }
-        
-        //Add/remove from a position in the list
-        linkedlist.add(0, "Additional Item");
-        linkedlist.remove(2);
-                System.out.println("\nFinal LinkedList:");
-        for(String counter: linkedlist) {
-                System.out.println(counter);
-        }
-        
-        System.out.println("\n*****Queue Example*****");
-        //Queue example
-        Queue<String> familyQueue = new LinkedList<String>();
-        
-        //Add Family members to queue
-        familyQueue.add("Kristopher");
-        familyQueue.add("Margaret");
-        familyQueue.add("Landon");
-        familyQueue.add("Elsira");
-        
-        //Display queue
-                System.out.println("\nQueue LinkedList:");
-        for(String counter: familyQueue) {
-                System.out.println(counter);
-        }
-        
-        //Remove Family members from the queue
-        System.out.println("\nRemoved element: " + familyQueue.poll());
-        
-        //Return first element in the queue
-        System.out.println("\nHead element: " + familyQueue.peek());
-        
-        System.out.println("\n*****HashMap Example*****");
-        //HashMap Example
-        HashMap<Integer, String> familyMap = new HashMap<Integer, String>();
-        
-        //Add Family members and map key
-        familyMap.put(39, "Kristopher");
-        familyMap.put(37, "Margaret");
-        familyMap.put(12, "Landon");
-        familyMap.put(4, "Elsira");
-        
-        //display map contents using iterator
-        Set set = familyMap.entrySet();
-        Iterator iterator = set.iterator();
-        System.out.println("\nKey & Value output:");
-        while (iterator.hasNext()) {
-            Map.Entry mapEntry = (Map.Entry)iterator.next();
-            System.out.println("Key: " + mapEntry.getKey() + " & value: " + mapEntry.getValue());           
-        }
-        
-        //Get specific element based on key
-        System.out.println("\nSpecific Value: " + familyMap.get(37));
-        
-        System.out.println("\n*****HashSet Example*****");
-        //HashSet Example
-        HashSet<String> animalHash = new HashSet<String>();
-        
-        //adding items to the list
-        animalHash.add("Cat");
-        animalHash.add("Dog");
-        animalHash.add("Bird");
-        animalHash.add("Fish");
-        animalHash.add("Lizard");
-        
-        //adding duplicate values
-        animalHash.add("Cat");
-        animalHash.add("Fish");
-        
-        //adding null values
-        animalHash.add(null);
-        
-        //display contents of list
-                System.out.println("\nAnimal HashSet List:");
-        for(String counter: animalHash) {
-                System.out.println(counter);
-        }
-    }   
+
+        // Queue example
+        System.out.println("\nQueue Example of German Cars:");
+        Queue<String> warteschlange = new LinkedList<>();
+
+        warteschlange.add("BMW");
+        warteschlange.add("Volkswagen");
+        warteschlange.add("Merceded");
+        warteschlange.add("Audi");
+        warteschlange.add("Porsche");
+
+        warteschlange.forEach(name -> {
+           System.out.println(name);
+        });
+
+    }
 }
